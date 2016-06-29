42c21f0643127b8611ff348f6ba5420fef1ad18c
Xhien Yi Tan <XavierTanXY@Xaviers-MBP.gateway>
2016-06-29 18:14:19 +0800

    added sound effects and parsing csv file

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,32 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Xhien Yi Tan on 29/06/2016.
+//  Copyright © 2016 Xavier TanXY. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+  
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.layer.cornerRadius = 5.0
+        
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLbl.text = pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
